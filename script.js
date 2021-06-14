e9f1869cb6f98e0b6494fd8d653abba990da17d4
Alberto MOGA <moga.alberto@hotmail.com>
2021-06-14 15:45:47 +0300

    preliminary_phase_1

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const panels = document.querySelectorAll('.panel');
+const goToTopButton = document.getElementById('go-to-top');
+const body = document.getElementById('body');
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
+});
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    });
+};
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+    if (window.pageYOffset > 200) {
+        goToTopButton.classList.add('active');
+    } else {
+        goToTopButton.classList.remove('active');
+    }
+};
+
+
+window.onresize = () => {
+    document.getElementById('top').scrollIntoView();
+    setTimeout(() => {location.reload();}, 1000);
+}
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
